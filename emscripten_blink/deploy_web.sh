75455c2a4fde613753684025a9ad6201209344c8
robalb <11277482+robalb@users.noreply.github.com>
2024-08-28 16:46:39 +0200

    tweaks

--- emscripten_blink/deploy_web.sh
+++
@@ -1,6 +0,0 @@
-
-cd blink && emmake make o//blink/blinkenlib.html
-
-cp ./o/blink/blinkenlib.wasm ../../svelte_blinkenweb/src/assets/
-cp ./o/blink/blinkenlib.js ../../svelte_blinkenweb/src/assets/
-cp ./o/blink/blinkenlib.html ../../svelte_blinkenweb/src/assets/
--- emscripten_blink/get_blink.sh
+++
@@ -1,4 +0,0 @@
-
-cp -rd /home/al/projects/blinkenlights/blink/ .
-
-emconfigure ./configure --disable-all LDFLAGS="-sENVIRONMENT=web"
