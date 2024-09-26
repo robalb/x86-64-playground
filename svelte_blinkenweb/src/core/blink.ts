85d5d4c0f90170cfc939a4e4b7b2e5b23a70415e
robalb <11277482+robalb@users.noreply.github.com>
2024-09-26 22:56:41 +0200

    preparing for test

--- svelte_blinkenweb/src/core/blink.ts
+++ svelte_blinkenweb/src/core/blink.ts
@@ -13,36 +13,73 @@ let protocolMap = {
 }
 
 let signals = {
-  "SIGINT":		2,	/* Interactive attention signal.  */
-  "SIGILL":	  4,	/* Illegal instruction.  */
-  "SIGABRT":	6,	/* Abnormal termination.  */
-  "SIGFPE":		8,	/* Erroneous arithmetic operation.  */
-  "SIGSEGV":	11,	/* Invalid access to storage.  */
-  "SIGTERM":	15,	/* Termination request.  */
-  "SIGHUP,":	1,	/* Hangup.  */
-  "SIGQUIT":	3,	/* Quit.  */
-  "SIGTRAP":	5,	/* Trace/breakpoint trap.  */
-  "SIGKILL":	9,	/* Killed.  */
-  "SIGPIPE":	13,	/* Broken pipe.  */
-  "SIGALRM":	14,	/* Alarm clock.  */
+    "SIGHUP": 1,
+    "SIGINT": 2,
+    "SIGQUIT": 3,
+    "SIGILL": 4,
+    "SIGTRAP": 5,
+    "SIGABRT": 6,
+    "SIGBUS": 7,
+    "SIGFPE": 8,
+    "SIGKILL": 9,
+    "SIGUSR1": 10,
+    "SIGSEGV": 11,
+    "SIGUSR2": 12,
+    "SIGPIPE": 13,
+    "SIGALRM": 14,
+    "SIGTERM": 15,
+    "SIGSTKFLT": 16,
+    "SIGCHLD": 17,
+    "SIGCONT": 18,
+    "SIGSTOP": 19,
+    "SIGTSTP": 20,
+    "SIGTTIN": 21,
+    "SIGTTOU": 22,
+    "SIGURG": 23,
+    "SIGXCPU": 24,
+    "SIGXFSZ": 25,
+    "SIGVTALRM": 26,
+    "SIGPROF": 27,
+    "SIGWINCH": 28,
+    "SIGIO": 29,
+    "SIGPWR": 30,
+    "SIGSYS": 31
 }
 
 let signals_info = {
-  /* ISO C99 signals.  */
-    2: {"name": "SIGINT", "description": "Interactive attention signal."},
+    1: {"name": "SIGHUP", "description": "Hang up controlling terminal or process."},
+    2: {"name": "SIGINT", "description": "Interrupt from keyboard, Control-C."},
+    3: {"name": "SIGQUIT", "description": "Quit from keyboard, Control-\\."},
     4: {"name": "SIGILL", "description": "Illegal instruction."},
+    5: {"name": "SIGTRAP", "description": "Breakpoint for debugging."},
     6: {"name": "SIGABRT", "description": "Abnormal termination."},
-    8: {"name": "SIGFPE", "description": "Erroneous arithmetic operation."},
-    11: {"name": "SIGSEGV", "description": "Invalid access to storage."},
-    15: {"name": "SIGTERM", "description": "Termination request."},
-  /* Historical signals specified by POSIX. */
-    1: {"name": "SIGHUP", "description": "Hangup."},
-    3: {"name": "SIGQUIT", "description": "Quit."},
-    5: {"name": "SIGTRAP", "description": "Trace/breakpoint trap."},
-    9: {"name": "SIGKILL", "description": "Killed."},
-    13: {"name": "SIGPIPE", "description": "Broken pipe."},
-    14: {"name": "SIGALRM", "description": "Alarm clock."}
+    7: {"name": "SIGBUS", "description": "Bus error."},
+    8: {"name": "SIGFPE", "description": "Floating-point exception."},
+    9: {"name": "SIGKILL", "description": "Forced-process termination."},
+    10: {"name": "SIGUSR1", "description": "Available to processes."},
+    11: {"name": "SIGSEGV", "description": "Invalid memory reference."},
+    12: {"name": "SIGUSR2", "description": "Available to processes."},
+    13: {"name": "SIGPIPE", "description": "Write to pipe with no readers."},
+    14: {"name": "SIGALRM", "description": "Real-timer clock."},
+    15: {"name": "SIGTERM", "description": "Process termination."},
+    16: {"name": "SIGSTKFLT", "description": "Coprocessor stack error."},
+    17: {"name": "SIGCHLD", "description": "Child process stopped or terminated or got a signal if traced."},
+    18: {"name": "SIGCONT", "description": "Resume execution, if stopped."},
+    19: {"name": "SIGSTOP", "description": "Stop process execution, Ctrl-Z."},
+    20: {"name": "SIGTSTP", "description": "Stop process issued from tty."},
+    21: {"name": "SIGTTIN", "description": "Background process requires input."},
+    22: {"name": "SIGTTOU", "description": "Background process requires output."},
+    23: {"name": "SIGURG", "description": "Urgent condition on socket."},
+    24: {"name": "SIGXCPU", "description": "CPU time limit exceeded."},
+    25: {"name": "SIGXFSZ", "description": "File size limit exceeded."},
+    26: {"name": "SIGVTALRM", "description": "Virtual timer clock."},
+    27: {"name": "SIGPROF", "description": "Profile timer clock."},
+    28: {"name": "SIGWINCH", "description": "Window resizing."},
+    29: {"name": "SIGIO", "description": "I/O now possible."},
+    30: {"name": "SIGPWR", "description": "Power supply failure."},
+    31: {"name": "SIGSYS", "description": "Bad system call."}
 }
+
 
 /**
 * A javascript wrapper for the blink x86-64 emulator.
@@ -124,8 +161,14 @@ export default class Blink{
     this.state = state;
   }
 
+  /**
+  * This callback is called from the wasm code
+  * whenever a terminating signal is raised
+  *
+  * SIGTRAP is the only signal that does not indicate
+  * a program stop.
+  */
   #extern_c__signal_callback(sig: number, code: number){
-    //all signals except sigtrap indicate that the program terminated
     if(sig != signals.SIGTRAP){
       this.#setState(this.states.PROGRAM_STOPPED);
     }
