72cead03dcd8f186db23457ef26bbb5711ef9f5a
robalb <11277482+robalb@users.noreply.github.com>
2024-11-01 16:25:18 +0100

    completed share popup

---
+++ svelte_blinkenweb/src/components/icons/Clipboard.svelte
@@ -0,0 +1,30 @@
+
+<!--@component
+  Svg Icon
+  By default this icon will have a fill of the default foreground color.
+
+  To override a color, use the style attribute with a theme css variable:
+  ```tsx
+    <Icon style="fill:var(--theme-primary);" />
+  ```
+  
+  By default, this icon will have a height and width of 24px.
+  To override:
+  ```tsx
+    <Icon width="12px" height="12px" />
+  ```
+-->
+<svg xmlns="http://www.w3.org/2000/svg"
+  height="24px"
+  viewBox="0 -960 960 960"
+  width="24px"
+  class="default-fill"
+  {...$$props}
+><path d="M360-240q-33 0-56.5-23.5T280-320v-480q0-33 23.5-56.5T360-880h360q33 0 56.5 23.5T800-800v480q0 33-23.5 56.5T720-240H360Zm0-80h360v-480H360v480ZM200-80q-33 0-56.5-23.5T120-160v-560h80v560h440v80H200Zm160-240v-480 480Z"/></svg>
+<style>
+.default-fill{
+  fill: var(--theme-text-fg-contrasting);
+}
+</style>
+
+
