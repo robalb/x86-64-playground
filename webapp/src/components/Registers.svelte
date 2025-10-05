07edce98cff1da14ec31c133ef90ee8b54c37bde
robalb <11277482+robalb@users.noreply.github.com>
2025-10-05 16:42:09 +0200

    Refactor code formatting registers component

--- webapp/src/components/Registers.svelte
+++ webapp/src/components/Registers.svelte
@@ -1,51 +1,54 @@
 <script>
-import {blinkStore, manual_render} from '../core/store'
+import { blinkStore, manual_render } from "../core/store";
 
-let blink = blinkStore.getInstance()
+const blink = blinkStore.getInstance();
 
 let registers = [
-  {name: "rax", updated: false, str: "0x00"},
-  {name: "rbx", updated: false, str: "0x00"},
-  {name: "rcx", updated: false, str: "0x00"},
-  {name: "rdx", updated: false, str: "0x00"},
+	{ name: "rax", updated: false, str: "0x00" },
+	{ name: "rbx", updated: false, str: "0x00" },
+	{ name: "rcx", updated: false, str: "0x00" },
+	{ name: "rdx", updated: false, str: "0x00" },
 
-  {name: "rsp", updated: false, str: "0x00"},
-  {name: "rbp", updated: false, str: "0x00"},
-  {name: "rsi", updated: false, str: "0x00"},
-  {name: "rdi", updated: false, str: "0x00"},
-  {name: "rip", updated: true, str: "0x00"},
+	{ name: "rsp", updated: false, str: "0x00" },
+	{ name: "rbp", updated: false, str: "0x00" },
+	{ name: "rsi", updated: false, str: "0x00" },
+	{ name: "rdi", updated: false, str: "0x00" },
 
-  {name: "r8 ", updated: false, str: "0x00"},
-  {name: "r9 ", updated: false, str: "0x00"},
-  {name: "r10", updated: false, str: "0x00"},
-  {name: "r11", updated: false, str: "0x00"},
-  {name: "r12", updated: false, str: "0x00"},
-  {name: "r13", updated: false, str: "0x00"},
-  {name: "r14", updated: false, str: "0x00"},
-  {name: "r15", updated: false, str: "0x00"},
-]
+	{ name: "r8 ", updated: false, str: "0x00" },
+	{ name: "r9 ", updated: false, str: "0x00" },
+	{ name: "r10", updated: false, str: "0x00" },
+	{ name: "r11", updated: false, str: "0x00" },
+	{ name: "r12", updated: false, str: "0x00" },
+	{ name: "r13", updated: false, str: "0x00" },
+	{ name: "r14", updated: false, str: "0x00" },
+	{ name: "r15", updated: false, str: "0x00" },
+	{ name: "rip", updated: true, str: "0x00" },
+];
 
-function updateRegisters(){
-  if(!(blink.state == blink.states.PROGRAM_RUNNING ||
-    blink.state == blink.states.PROGRAM_STOPPED)) return;
+function updateRegisters() {
+	if (
+		!(
+			blink.state === blink.states.PROGRAM_RUNNING ||
+			blink.state === blink.states.PROGRAM_STOPPED
+		)
+	)
+		return;
 
-  for(let reg of registers){
-    let new_str = blink.m.stringReadU64(reg.name.trim())
-    if(new_str == reg.str){
-      reg.updated = false;
-    }
-    else{
-      reg.updated = true;
-      reg.str = new_str
-    }
-  }
-  //force svelte rendering
-  registers = registers;
+	for (const reg of registers) {
+		const new_str = blink.m.stringReadU64(reg.name.trim());
+		if (new_str === reg.str) {
+			reg.updated = false;
+		} else {
+			reg.updated = true;
+			reg.str = new_str;
+		}
+	}
+	//force svelte rendering
+	registers = registers;
 }
 
 //rerender registers on machine step
 $: $manual_render && updateRegisters();
-
 </script>
 
 <section class="registers">
